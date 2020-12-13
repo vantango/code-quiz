f936e72d417afcbc44d2ae388539ca8c8c332855
Aleks Kimball-Moreno <alekskimball@gmail.com>
2020-12-13 10:01:11 -0800

    center aligned content in jumbotron and added high scores hyperlink

--- script.js
+++ script.js
@@ -4,14 +4,44 @@
 // TODO: Create a History Quiz!
 var quizList = [
     {
-        question: "question 1",
-        choices: ["a", "b", "c", "d"],
-        correctAnswer: "a"
+        question: "Who was the 27th President of the United States?",
+        choices: ["William Howard Taft", "Franklin Pierce", "Theodore Roosevelt", "Woodrow Wilson"],
+        correctAnswer: "William Howard Taft"
     }
     {
-        question: "question 2",
-        choices: ["a", "b", "c", "d"],
-        correctAnswer: "c"
+        question: "What year did the Berlin Conference take place?",
+        choices: ["1867", "1995", "1884", "1721"],
+        correctAnswer: "1884"
+    }
+    {
+        question: "Which ancient empire ruled the British Isles between 208-11 AD?",
+        choices: ["Greeks", "Byzantines", "Romans", "Huns"],
+        correctAnswer: "Romans"
+    }
+    {
+        question: "Who invented the world wide web?",
+        choices: ["Brendan Eich", "Barbara Liskov", "Alan Turing", "Tim Berners-Lee"],
+        correctAnswer: "Tim Berners-Lee"
+    }
+    {
+        question: "What year did Colombia gain it's independence?",
+        choices: ["1810", "1782", "1656", "1938"],
+        correctAnswer: "1810"
+    }
+    {
+        question: "Which Tsar of Russia freed the serfs?",
+        choices: ["Ivan V", "Alexander II", "Peter III", "Catherine II"],
+        correctAnswer: "Alexander II"
+    }
+    {
+        question: "Who launched the counter-offensive that initiated the Battle of the Bulge in WWII?",
+        choices: ["UK", "Russia", "United States", "Germany"],
+        correctAnswer: "Germany"
+    }
+    {
+        question: "Which Spanish painter is known for the masterpiece 'Last Meninas?'",
+        choices: ["Salvador Dali", "Pablo Picasso", "Diego Velasquez", "Francisco de Goya"],
+        correctAnswer: "Diego Velasquez"
     }
 ]
 
@@ -31,7 +61,7 @@ var timeEl = document.getElementById("clock")
 function setTime() {
     timerInterval = setInterval(function () {
         secondsLeft--;
-        timeEl.textContent = secondsLeft + " seconds left till colorsplosion.";
+        timeEl.textContent = secondsLeft + "Round over!";
 
         if (secondsLeft === 0) {
             clearInterval(timerInterval);
